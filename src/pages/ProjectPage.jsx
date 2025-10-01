abed2ad327e28a7a2e60c2fbdf97bf52bd9e45b4
Nate Levi <nathan.levi8@gmail.com>
2025-10-01 18:01:23 -0400

    added cb basketball project and reordered list

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -4,35 +4,42 @@ const ProjectPage = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <ProjectCard 
-        projectName="Project 1: Tropicuban Cafe" 
-        description="A responsive React/Vite website styled with TailwindCSS and powered by Firebase, built as a ordering platform for a local Cuban cafe and deployed on Netlify to showcase my full‑stack web development skills." 
-        image="project2.png"
-        link = "https://github.com/NateLevi/TropicubanCafe"
-        liveLink = "https://tropicubancafe.netlify.app/"
-      />
-      <ProjectCard 
-        projectName="Project 2: NBA Draft Hub" 
-        description="Draft Hub I created for the Dallas Mavericks Front Office Decision Makers for the NBA Draft." 
-        image="mavsproject.png"
-        link = "https://github.com/NateLevi/NBADraftHub"
-        liveLink = "https://nbadrafthubapp.netlify.app/"
-      />
-      <ProjectCard 
-        projectName="Project 3: DailyGolazo" 
-        description="A web platform aggregating daily match highlights from Europe's top 5 football leagues. Users can search and watch curated highlights from each league's latest matches." 
-        image="project1.png"
-        link = "https://github.com/NateLevi/DailyGolazo"
-        liveLink = "https://dailygolazo.onrender.com/"
-      />
-      <ProjectCard 
-        projectName="Project 4: Cs Manager System" 
-        description= 'Cs Manager System is an application I developed while working as a sales representative for a Verizon retailer, Cellular Sales. We used a rotation system called "swings" to fairly distribute walk-in customers among reps, but tracking it manually caused confusion. Additionally, we lacked a modern inventory and supply ordering system. I built this app to solve both problems.'
+        projectName="Project 1: Cs Manager System" 
+        description='Cs Manager System is an application I developed while working as a sales representative for a Verizon retailer, Cellular Sales. We used a rotation system called "swings" to fairly distribute walk-in customers among reps, but tracking it manually caused confusion. Additionally, we lacked a modern inventory and supply ordering system. I built this app to solve both problems.'
         image="csswingproject.png"
         link="https://github.com/NateLevi/CsSwingManager"
         liveLink="https://csswingmanager.netlify.app/"
       />
       <ProjectCard 
-        projectName="Project 5: Philosopher Api" 
+        projectName="Project 2: NBA Draft Hub" 
+        description="Draft Hub I created for the Dallas Mavericks Front Office Decision Makers for the NBA Draft." 
+        image="mavsproject.png"
+        link="https://github.com/NateLevi/NBADraftHub"
+        liveLink="https://nbadrafthubapp.netlify.app/"
+      />
+      <ProjectCard 
+        projectName="Project 3: DailyGolazo" 
+        description="A web platform aggregating daily match highlights from Europe's top 5 football leagues. Users can search and watch curated highlights from each league's latest matches." 
+        image="project1.png"
+        link="https://github.com/NateLevi/DailyGolazo"
+        liveLink="https://dailygolazo.onrender.com/"
+      />
+      <ProjectCard 
+        projectName="Project 4: College Basketball Player Development App" 
+        description="This application gives coaches and players data to help improve player development based on performance metrics and statistics." 
+        image="cbbasketballapp.png"
+        link="https://github.com/NateLevi/CollegeBasketballDevelopApp"
+        liveLink="https://collegebbdevelopapp.netlify.app/"
+      />
+      <ProjectCard 
+        projectName="Project 5: Tropicuban Cafe" 
+        description="A responsive React/Vite website styled with TailwindCSS and powered by Firebase, built as a ordering platform for a local Cuban cafe and deployed on Netlify to showcase my full-stack web development skills." 
+        image="project2.png"
+        link="https://github.com/NateLevi/TropicubanCafe"
+        liveLink="https://tropicubancafe.netlify.app/"
+      />
+      <ProjectCard 
+        projectName="Project 6: Philosopher Api" 
         description="A RESTful API providing comprehensive data on historical philosophers, including their key works, philosophical concepts, famous quotes, biographical facts, and influence on modern thought. Access structured data for educational and research purposes." 
         image="project4.jpg"
         link="https://github.com/NateLevi/philosopher-names-api"
